6e9234ed1ec734443a9515be0ba6e3c3058cf8c0
dankelleher <dankelleher@yahoo.com>
2024-09-04 13:51:20 +0200

    Drop obsolete token usage protocol

--- protocol/token-usage-settlement/program/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { delegate } from "./delegateTokens";
-export { draw } from "./draw";
-export { registerUsage, RegisterUsageParams } from "./usage";
-export {
-  deriveDelegateAndBumpSeed,
-  providerFor,
-  ExtendedCluster,
-  getClusterUrl,
-} from "./lib/util";
